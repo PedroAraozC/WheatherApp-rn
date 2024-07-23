66a2fb269b944e8c465418a088066432202f4869
PedroAraozC <pedroaraozc@gmail.com>
2024-07-23 12:56:23 -0300

    .env de mierda

--- index.js
+++ index.js
@@ -6,42 +6,49 @@ import {
   StyleSheet,
   ActivityIndicator,
   ScrollView,
-  refreshControl,
   RefreshControl,
+  Image,
 } from "react-native";
 import React, { useEffect, useState } from "react";
 import * as Location from "expo-location";
+import { APIKEY } from '@babel/core';
 
-const openWeatherKey = "a1cd95af7f3524240c00c116e9261781";
-let url = `https://api.openweathermap.org/data/2.5/onecall?&units=metric&exclude=minutely&appid=${openWeatherKey}`;
+const openWeatherKey =  APIKEY;
 
 const Weather = () => {
   const [forecast, setForecast] = useState(null);
   const [refreshing, setRefreshing] = useState(false);
-  let location;
 
+  // "a1cd95af7f3524240c00c116e9261781"
   const loadForecast = async () => {
     setRefreshing(true);
     // PREGUNTA POR PERMISOS DE UBICACION
     const { status } = await Location.requestForegroundPermissionsAsync();
 
-    if (status !== ""(Alert.alert("Permiso de ubicación denegada")))
-      // TOMA LA UBICACION ACTUAL
+    if (status !== "granted") {
+      Alert.alert("Permiso de ubicación denegada");
+      setRefreshing(false);
+      return;
+    }
+    // TOMA LA UBICACION ACTUAL
+    const location = await Location.getCurrentPositionAsync({
+      enableHighAccuracy: true,
+    });
 
-      location = await Location.getCurrentPositionAsync({
-        enableHighAccuracy: true,
-      });
+    // Construye la URL con la ubicación actual
+    const fullUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${location.coords.latitude}&lon=${location.coords.longitude}&units=metric&exclude=minutely&APPID=${openWeatherKey}`;
 
-    //Toma la informacion de la api de openWeatherMap
-    const response = await fetch(
-      `${url}&alt=${location.coords.latitude}&lon=${location.coords.longitude}`
-    );
-    const data = await response.json(); //convierte la respuesta en un json
+    console.log("API Key:", openWeatherKey);
+    console.log("URL:", fullUrl);
+    // Toma la informacion de la api de openWeatherMap
+    const response = await fetch(fullUrl);
+    const data = await response.json(); // convierte la respuesta en un json
+    console.log("response", data.main);
 
     if (!response.ok) {
       Alert.alert("Error", "Algo salió mal :("); // si no esta todo ok, muestra la alerta
     } else {
-      setForecast(data); //setea la informacion en el estado
+      setForecast(data); // setea la informacion en el estado
     }
     setRefreshing(false);
   };
@@ -53,12 +60,14 @@ const Weather = () => {
   if (!forecast) {
     return (
       <SafeAreaView style={styles.loading}>
-        <ActivityIndicator size="large" />
+        <ActivityIndicator size="xlarge" color="lightblue" />
       </SafeAreaView>
     );
   }
 
-  const current = forecast.current.weather[0];
+  const currentWeather = forecast.weather[0];
+
+  const iconUrl = `http://openweathermap.org/img/wn/${currentWeather.icon}@2x.png`;
 
   return (
     <SafeAreaView style={styles.container}>
@@ -71,13 +80,32 @@ const Weather = () => {
         }
         style={{ marginTop: 50 }}
       >
-        <Text style={styles.title}>Current Weather</Text>{" "}
-        <Text style={{ alignItems: "center", textAlign: "center" }}>
-          Your Location
+        <Text style={styles.title}>{forecast.name}</Text>
+        <Text
+          style={{ alignItems: "center", textAlign: "center", color: "#fff" }}
+        >
+          {forecast.sys.country}
         </Text>
-
-        <View>
-
+        <View style={styles.current}>
+          <View style={{ flexDirection: "column", alignItems: "center" }}>
+            <Image source={{ uri: iconUrl }} style={styles.largeIcon} />
+          </View>
+          <View>
+            <Text style={styles.currentTemp}>
+              {Math.round(forecast.main.temp)}°C
+            </Text>
+            <View style={styles.tiempoCont}>
+              <Text style={styles.tiempo}>
+                Mín {Math.round(forecast.main.temp_min)}°C
+              </Text>
+              <Text style={styles.tiempo}>
+                Máx {Math.round(forecast.main.temp_max)}°C
+              </Text>
+            </View>
+          </View>
+        </View>
+        <View style={styles.descriptionCont}>
+          <Text style={styles.description}>{currentWeather.description}</Text>
         </View>
       </ScrollView>
     </SafeAreaView>
@@ -86,7 +114,49 @@ const Weather = () => {
 
 export default Weather;
 
-
 const styles = StyleSheet.create({
-    
-})
+  container: { flex: 1, backgroundColor: "lightblue" },
+  title: {
+    textAlign: "center",
+    fontSize: 36,
+    fontWeight: "bold",
+    color: "white",
+  },
+  // loading: { paddingTop: 550, height: 200 },
+  current: {
+    flexDirection: "row",
+    alignItems: "center",
+    alignContent: "center",
+    justifyContent: "space-around",
+  },
+  largeIcon: {
+    width: 300,
+    height: 250,
+  },
+  currentTemp: {
+    fontSize: 42,
+    // height:250,
+    // width:'auto',
+    fontWeight: "bold",
+    textAlign: "center",
+    color: "white",
+  },
+  tiempoCont: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    width: 250,
+  },
+  tiempo: {
+    fontSize: 20,
+    color: "white",
+  },
+  description: {
+    color: "white",
+    fontSize: 20,
+    fontWeight: "bold",
+    textAlign:"center"
+  },
+  descriptionCont: {
+    marginBottom: 25,
+  },
+});
